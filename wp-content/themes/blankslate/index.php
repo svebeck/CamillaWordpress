fa7221d3662a4fc950000dfbafba6a16c40cd602
Stefan Svebeck <svebeck@gmail.com>
2016-09-15 22:50:22 +0200

    fixed some more

--- wp-content/themes/blankslate/index.php
+++ wp-content/themes/blankslate/index.php
@@ -1,37 +1,72 @@
 <?php get_header(); ?>
 <section id="content" role="main">
   <div class="splash">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/splash.png" />
   </div>
   <div class="row align-center">
-    <div class="column small-2">
-    	<div class="arrow">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/arrow.png" />
-		</div>
+    <div class="column small-4 arrow">
+		<img src="<?php echo get_template_directory_uri(); ?>/images/arrow.png" />
     </div>
   </div>
   <section id="projects">
-  	<h1>Senaste projekten</h1>
-  	<div class="row">
-  		<div class="column small-6 gray project-big">
+  	<h1 class="projects">Senaste projekten</h1>
+  	<div class="row collapse">
+  		<div class="column small-6 gray project-large">
 			<img src="<?php echo get_template_directory_uri(); ?>/images/lstk.png" />
   		</div>
-  		<div class="column small-6 project-big">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/lstk.png" />
+  		<div class="column small-6 project-small">
+  			<div class="padding-left">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/installation.png" />
 			</div>
-  		<div class="column small-6 project-big">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/lstk.png" />
+		</div>
+  		<div class="column small-6 project-small text-right">
+  			<div class="padding-right">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/installation.png" />
 			</div>
-  		<div class="column small-6 blue project-big">
+		</div>
+  		<div class="column small-6 blue project-large">
 			<img src="<?php echo get_template_directory_uri(); ?>/images/lstk.png" />
+		</div>
+  		<div class="column small-6 project-small text-right">
+  			<div>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/installation.png" />
 			</div>
-  		<div class="column small-6 project-big">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/lstk.png" />
+		</div>
+  		<div class="column small-6 project-small">
+  			<div class="padding-left">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/installation.png" />
 			</div>
-  		<div class="column small-6 project-big">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/lstk.png" />
-			</div>
+		</div>
   </section>
+  <section class="row collapse align-center quote">
+	<div class="column small-12">
+	  	<p class="quote-icon">“</p>
+	  	<p class="quote-text">Archicienem con corrum qui ipient ad unditatia is endictur remporeicius sitis cum sum dusti comnihitium nus aut et plit et ut dolutem ex et quam aut pra dolumqu atiorest.
+		</p>
+	</div>
+  </section>
+  <aside class="contact">
+  	<ul>
+	  	<li class="header">E-MAIL</li>
+		<li>camilla.karlsson@medieinstitutet.se</li>
+
+		<li class="header">BEHANCE</li>
+		<li>https://www.behance.net/c_karlssoned26</li>
+
+		<li class="header">TELFONNUMMER</li>
+		<li>070-6113064</li>
+	</ul>
+  </aside>
+  <section class="row collapse align-center about">
+	<div class="column small-12">
+	  	<h2>Archicienem con corrum qui ipient</h2>
+	  	<p>Archicienem con corrum qui ipient ad unditatia is endictur remporeicius sitis cum sum dusti comnihitium nus aut et plit et ut dolutem ex et quam aut pra dolumqu atiorest ma niatem illorem porercia sae. Et volent, qui doluptis int. Archicienem con corrum qui ipient ad unditatia is endictur remporeicius sitis cum sum dusti comnihitium nus aut et plit et ut dolutem ex et quam aut pra dolumqu atiorest ma niatem illorem porercia sae. Et volent, qui doluptis int
+
+		Archicienem con corrum qui ipient ad unditatia is endictur remporeicius sitis cum sum dusti comnihitium nus aut et plit et ut dolutem ex et quam aut pra dolumqu atiorest ma niatem illorem porercia sae. Et volent, qui doluptis int
+
+		Archicienem con corrum qui ipient ad unditatia is endictur remporeicius sitis cum sum dusti comnihitium nus aut et plit et ut dolutem ex et quam aut pra dolumqu atiorest ma niatem illorem porercia sae. Et volent, qui doluptis int.
+		</p>
+	</div>
+</section>
 <!-- Old Entry Code
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
